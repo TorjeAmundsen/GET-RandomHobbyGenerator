ae6fa0936fa11535ea5ebdc636da76ef6c3bf49c
Torje Amundsen <torjefrafen@hotmail.no>
2023-11-20 10:49:43 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HobbyGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] hobbies = {
+                " is now a sorcerer!",
+                " is now a professional artist!",
+                " is now a football player.",
+                " has become a rock climber!"
+            };
+            Console.WriteLine("Who would like a new hobby?");
+            string name = Console.ReadLine();
+            Random rng = new Random();
+            int randomIndex = rng.Next(0, hobbies.Length);
+            string randomHobby = hobbies[randomIndex];
+            Console.WriteLine(name + randomHobby);
+        }
+    }
+}
